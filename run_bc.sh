64505f740c0f0eadaa54fee6b0733a82473b7b8a
arthur801031 <arthur801031@gmail.com>
2023-04-03 14:58:46 -0700

    Remove run_bc.sh script

--- run_bc.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python bc.py \
---bc_data=bc_data/chair_ingolf_0650.gail.p1.123_step_00017203200_2000_trajs.pkl \
---demo_path=demos/chair_ingolf/Sawyer_chair_ingolf_0650_1 \
---run_prefix=p1 \
---algo=gail \
---preassembled=0 \
---run_name_postfix=policy2 \
---wandb=True
